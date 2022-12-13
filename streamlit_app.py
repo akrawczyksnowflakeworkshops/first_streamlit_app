b50eceeed004cf40287524d32a6513bd25f54021
Arkadiusz <120383502+akrawczyksnowflakeworkshops@users.noreply.github.com>
2022-12-13 08:24:31 +0100

    Update streamlit_app.py
    add some changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 
 streamlit.title ('TITLE: AKR test Streamlit')
 
-streamlit.header ('HEADER: AKR test Streamlit')
-streamlit.text ('LINE 1 AKR test Streamlit')
-streamlit.text ('LINE 2 AKR test Streamlit')
-streamlit.text ('LINE 3 AKR test Streamlit')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
