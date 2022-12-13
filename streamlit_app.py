08e27d51fe97b2f83bebfba7ce426a22763d35f5
Arkadiusz <120383502+akrawczyksnowflakeworkshops@users.noreply.github.com>
2022-12-13 08:06:35 +0100

    Update streamlit_app.py
    added some lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title ('AKR test Streamlit')
+streamlit.header ('HEADER AKR test Streamlit')
+streamlit.title ('LINE 1 AKR test Streamlit')
+streamlit.title ('LINE 2 AKR test Streamlit')
+streamlit.title ('LINE 3 AKR test Streamlit')
+streamlt.title ('LINE 4 AKR test Streamlit')
